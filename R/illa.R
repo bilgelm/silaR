603c812f7ed1f0e4111deee9e04191f0dbe36ff3
hmbruzzone <hbruzzone@wisc.edu>
2025-10-30 10:46:25 -0500

    fixed n() function

--- R/illa.R
+++ R/illa.R
@@ -38,7 +38,7 @@ illa <- function(df, dt, val0, maxi, skern) {
   t <- df %>%
     dplyr::arrange(subid, age) %>%
     dplyr::group_by(subid) %>% 
-    dplyr::filter(n()>1) %>%
+    dplyr::filter(dplyr::n()>1) %>%
     dplyr::ungroup() %>%
     dplyr::group_nest(subid, .key = "data") %>%
     dplyr::mutate(
